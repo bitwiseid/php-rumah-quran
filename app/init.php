ab6641c6de79ee1c7a87c5db4797396d6c9b3ec9
alamsyah-d <alimuddin32198@gmail.com>
2025-01-06 16:48:50 +0700

    🎉 Initial commit for all feature

--- app/init.php
+++ app/init.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+require_once 'config/config.php';
 
 require_once 'core/App.php';
 require_once 'core/AdminApp.php';
@@ -8,4 +9,3 @@ require_once 'core/Controller.php';
 require_once 'core/Flasher.php';
 require_once 'core/Model.php';
 require_once 'core/Database.php';
-require_once 'config/config.php';
